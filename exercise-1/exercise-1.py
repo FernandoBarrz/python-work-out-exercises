6e7fbddd5c673caac92930ba9163ed9465bb553f
fernando-ubuntu <barrioslui02@gmail.com>
2020-07-04 23:15:18 -0500

    Add description and solution V.1.0

---
+++ exercise-1/exercise-1.py
@@ -0,0 +1,32 @@
+# Exercise number 1 - Python WorkOut
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.8.2 64-bit
+
+# Dependencies 
+import random
+
+# Main function with no arguments
+def guessing_game():
+    '''This function generates a random number and ask for guessing it'''
+    random_number = random.randint(0, 100)
+    
+    while True:
+        user_input = int(input('Guess a number!\n'))
+        if user_input > random_number:
+            print("Too high\n")
+            
+        if(user_input < random_number):
+            print('Too low\n')
+            
+        if(user_input == random_number):
+            print('Just right\n')
+            break    
+    
+
+
+
+guessing_game()
+
+
+
+    
