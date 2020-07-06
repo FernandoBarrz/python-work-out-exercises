32a884a28e0154f86afd0a5a02eba4c70414654f
fernando-ubuntu <barrioslui02@gmail.com>
2020-07-05 22:02:20 -0500

    Add the solution and the description to the second exercise

---
+++ exercise-2/exercise-2.py
@@ -0,0 +1,12 @@
+# Exercise number 1 - Python WorkOut
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.8.2 64-bit
+
+def mysum(*numbers): # The "splat" operator is used when we need an arbitrary amount of arguments
+    output = 0
+    for i in args: # It returns a tuple
+        output += i  
+    return output
+print(mysum(4,6,7,8,5,4,3))
+    
+
