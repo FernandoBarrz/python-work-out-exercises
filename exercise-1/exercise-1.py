c9098028a6f01ba9412d7e0c97308cb12867e5df
Fernando B <barrioslui02@gmail.com>
2020-11-19 20:28:45 -0600

    Implement PEP-8 guideline

--- exercise-1/exercise-1.py
+++ exercise-1/exercise-1.py
@@ -5,28 +5,26 @@
 # Dependencies 
 import random
 
+
 # Main function with no arguments
 def guessing_game():
-    '''This function generates a random number and ask for guessing it'''
+    """
+        This function generates a random number and ask for guessing it
+    """
     random_number = random.randint(0, 100)
-    
+
     while True:
+
         user_input = int(input('Guess a number!\n'))
         if user_input > random_number:
             print("Too high\n")
-            
-        if(user_input < random_number):
-            print('Too low\n')
-            
-        if(user_input == random_number):
-            print('Just right\n')
-            break    
-    
 
+        if user_input < random_number:
+            print('Too low\n')
+
+        if user_input == random_number:
+            print('Just right\n')
+            break
 
 
 guessing_game()
-
-
-
-    
