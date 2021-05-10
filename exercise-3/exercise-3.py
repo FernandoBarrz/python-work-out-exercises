84f68d9ccdb4b0da95c2d50f4830c333e38049b4
Fernando Barrios <barrioslui02@gmail.com>
2021-05-09 20:54:43 -0500

    Improve exercise-3 solution algorithm

--- exercise-3/exercise-3.py
+++ exercise-3/exercise-3.py
@@ -5,17 +5,16 @@
 def run_timing():
     """Asks the user repeatedly for numeric input. Prints the average time an
 d number of runs."""
+    number_of_runs = 0
     total_time = 0
-    total_runs = 0
-    while(True):
-        try:
-            time_to_run = float(input("Enter 10 km run time:  "))
-            total_time += time_to_run
-        except ValueError as e:
+    while True:
+        one_run = input('Enter 10 km run time: ')
+        if not one_run:
             break
-            print(f'You entered a especial character or string: {e}')
-        total_runs += 1
-        average_time = total_time / total_runs
-    return f'Average of {average_time:.1f}, over {total_runs} runs'
+        number_of_runs += 1
+        total_time += float(one_run)
+        
+    average_time = total_time / number_of_runs
 
+    print(f'Average of {average_time}, over {number_of_runs} runs')
 run_timing()
