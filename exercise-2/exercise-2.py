d7c10b466be60a18ce657f591ecc594700922465
Fernando Barrios <barrioslui02@gmail.com>
2021-05-09 20:25:54 -0500

    Improve "exercise-2" solution algorithm
    Improve the way the algorithm sum up the elements in the array

--- exercise-2/exercise-2.py
+++ exercise-2/exercise-2.py
@@ -3,8 +3,8 @@
 # Language: Python3 3.8.2 64-bit
 
 def my_sum(*numbers): # The "splat" operator is used when we need an arbitrary amount of arguments
-    output = 0
-    for i in numbers: # It returns a tuple
+    output = numbers[0]
+    for i in numbers[1:]: # It returns a tuple
         output += i  
     return output
 print(my_sum(4, 6))
