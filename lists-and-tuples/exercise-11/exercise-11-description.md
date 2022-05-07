13197e2be54f4d1e86ba575ca3df6876acd98e91
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-05-06 23:00:09 -0500

    Add solution for exercise-11.py

---
+++ lists-and-tuples/exercise-11/exercise-11-description.md
@@ -0,0 +1,6 @@
+# Exercise 11 : 
+
+## For this excercise
+Write  a function (alphabetize_names), that assumes the existence of a PEOPLE constant defined as shown in the code.
+The function should return the list of dicts, but sorted by last name and then by first name.
+
---
+++ lists-and-tuples/exercise-11/exercise-11.py
@@ -0,0 +1,43 @@
+# Exercise number 11 - Python Workout Book
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.10.2 64-bit on Mac M1
+
+
+from importlib.util import find_spec
+
+import operator
+
+PEOPLE = [
+            {'first':'Reuven',
+                'last':'Lerner',
+                'email':'reuven@lerner.co.il'
+            },
+            {'first':'Donald', 
+                'last':'Trump', 
+                'email':'president@whitehouse.gov'
+            },
+            {'first':'Vladimir', 
+                'last':'Putin',
+                'email':'president@kremvax.ru'
+            }
+        ] 
+
+def alphabetize_names(sequence_to_sort):
+    '''
+    Return the list of dicts, but sorted by last name and then by first name.
+    '''
+    # The "Key" parameter to "sorted" gets a function, whose result indicates how we'll sort.
+    return sorted(sequence_to_sort, key= operator.itemgetter('last', 'first'))
+
+for e in alphabetize_names(PEOPLE):
+    print(e["last"], e["first"])
+
+
+    
+
+    
+
+
+
+
+
