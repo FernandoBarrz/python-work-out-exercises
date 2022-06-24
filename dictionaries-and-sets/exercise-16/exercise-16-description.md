d8ccb7d954a81b3c757659aae2fc6201e1af489d
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-06-23 23:03:48 -0500

    Add solution to exercise-16

--- dictionaries-and-sets/exercise-16/exercise-16-description.md
+++ dictionaries-and-sets/exercise-16/exercise-16-description.md
@@ -1,7 +1,9 @@
 # Exercise 16 : Dictdiff
 
 ## For this excercise
-
+Write a function _dictdiff_, that takes dicts as arguments.
+The function returns a mew dict that expresses the difference between the two dicts.
+* If no differences return an empty dict.
 
 
 
--- dictionaries-and-sets/exercise-16/exercise-16.py
+++ dictionaries-and-sets/exercise-16/exercise-16.py
@@ -1,33 +1,26 @@
-# Exercise number 15 - Python Workout 50 essential exercises book
+# Exercise number 16 - Python Workout 50 essential exercises book
 # Author: Barrios Ramirez Luis Fernando
 # Language: Python3 3.10.2 64-bit on Mac M1
 
 
-rainfall = {}
 
-def get_rainfall():
+def dictdiff(first, second):
+    output = {}
+    all_keys = first.keys() | second.keys()
 
-    while True:
-        city_name = input("Enter a city name: ")
-        if not city_name:
-            break
-        
-        if city_name in rainfall:
-            print(f'{city_name}:  {rainfall[city_name]}')
+    for key in all_keys:
+        first_val = first.get(key, None)
+        second_val = second.get(key, None)
+        if first_val != second_val:
+            output[key] = [first_val, second_val]
 
-        
-        new_city_rainfall = input("How much rain has fallen in the city? (in milimeters): ")
-        if not new_city_rainfall:
-            break
-        rainfall[city_name] = float(new_city_rainfall)
-
-        
-    for city, rain in rainfall.items():
-        print(f'{city}:  {rain}')
+    return output
 
 
-get_rainfall()
+a = {'a': 1, 'b': 2, 'c': 3}
+b = {'a': 1, 'b': 2, 'c': 4}
 
+print(dictdiff(a, b))
     
 
 
