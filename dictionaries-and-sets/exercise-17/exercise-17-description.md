3364523c9f75c5099cf5d584404af8a0b2ba9c2b
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-06-25 13:47:33 -0500

    Added solution to exercise-17

---
+++ dictionaries-and-sets/exercise-17/exercise-17-description.md
@@ -0,0 +1,11 @@
+# Exercise 17 : How many different numbers
+
+## For this excercise
+Given a list of integers. Write a function __how_many_different_numbers__, that takes a single list of integers and returns the number of different integers it cointains. 
+
+```python
+numbers = [1, 2, 3, 1, 2, 3, 4, 1]
+```
+
+
+
---
+++ dictionaries-and-sets/exercise-17/exercise-17.py
@@ -0,0 +1,23 @@
+# Exercise number 17 - Python Workout 50 essential exercises book
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.10.2 64-bit on Mac M1
+
+
+
+numbers = [1, 2, 3, 1, 2, 3, 4, 1]
+def how_many_different_numbers(arr):
+    get_set_from_arr = {*arr}
+
+    return len(get_set_from_arr)
+
+
+
+
+
+print(how_many_different_numbers(numbers))
+    
+
+
+
+
+
