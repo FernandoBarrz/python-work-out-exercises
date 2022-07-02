da11ba65160e3a0f7a3eeecbb694aabcf52f4ad3
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-01 23:51:50 -0500

    Added starting files for exercise 23

---
+++ files/exercise-23/exercise-23-description.md
@@ -0,0 +1,7 @@
+# Exercise 23 : JSON
+
+## For this excercise
+Write a function, __print_scores__, that takes a directory name as an argument
+and prints a summary of the student scores it finds.
+The function should print the highest, lowest, and average test scores for each subject
+in each class.
---
+++ files/exercise-23/exercise-23.py
@@ -0,0 +1,31 @@
+# Exercise number 23 - Python Workout 50 essential exercises book
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.10.2 64-bit on Mac M1
+
+from calendar import c
+import glob
+from importlib.util import decode_source
+import os
+from xml.etree.ElementInclude import DEFAULT_MAX_INCLUSION_DEPTH
+from zipfile import ZIP_MAX_COMMENT
+    
+def print_scores(dict_name):
+    list_files = glob.glob(f'{dict_name}/*.json')
+    for json_file in list_files:
+        with open() as file:
+            cxczmx,czxczx
+            
+def my_func():
+    pass
+            
+            
+            
+            
+
+
+
+
+
+
+
+
