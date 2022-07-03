a3812c5b49c60ef5861388c321edde9f677e79bf
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-03 00:26:27 -0500

    Added solution for exercise-22

--- files/exercise-23/exercise-23.py
+++ files/exercise-23/exercise-23.py
@@ -2,22 +2,37 @@
 # Author: Barrios Ramirez Luis Fernando
 # Language: Python3 3.10.2 64-bit on Mac M1
 
-from calendar import c
+
 import glob
-from importlib.util import decode_source
+import json
+
 import os
-from xml.etree.ElementInclude import DEFAULT_MAX_INCLUSION_DEPTH
-from zipfile import ZIP_MAX_COMMENT
+
     
-def print_scores(dict_name):
-    list_files = glob.glob(f'{dict_name}/*.json')
-    for json_file in list_files:
-        with open() as file:
-            cxczmx,czxczx
-            
-def my_func():
-    pass
-            
+def print_scores(dir_name):
+    scores = {}
+    for filename in glob.glob(f'{dir_name}/*.json'):
+        scores[filename] = {}
+        with open(filename) as infile:
+            for result in json.load(infile):
+                for subject, score in result.items():
+                    scores[filename].setdefault(subject, [])
+                    scores[filename][subject].append(score)
+    for one_class in scores:
+        print(one_class)
+        for subject, subject_scores in scores[one_class].items():
+            min_score = min(subject_scores)
+            max_score = max(subject_scores)
+            average_score = (sum(subject_scores) / len(subject_scores))
+
+            print(subject)
+            print(f'\tmin {min_score}')
+            print(f'\tmax {max_score}')
+            print(f'\taverage {average_score}')
+
+
+print_scores("scores")
+
             
             
             
