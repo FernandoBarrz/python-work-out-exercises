d2cb7d0c197018448eab31428e465212c0963cce
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-07 22:36:00 -0500

    Added solution for exercise-28

---
+++ FP-comprehensions/exercise-28/exercise-28-description.md
@@ -0,0 +1,7 @@
+# Exercise 28 : Join numbers
+
+## For this excercise
+
+Write a function, __join_numbers__, that takes a range of integers.
+This function should:
+* Return those numbers as a string, with commas between the numbers.
---
+++ FP-comprehensions/exercise-28/exercise-28.py
@@ -0,0 +1,19 @@
+# Exercise number 28 - Python Workout 50 essential exercises book
+# Author: Barrios Ramirez Luis Fernando
+# Language: Python3 3.10.2 64-bit on Mac M1
+
+
+
+
+def join_numbers(num_range: int) -> str:
+    return ", ".join(str(num) for num in range(num_range))
+
+
+print(join_numbers(15))
+
+
+
+
+
+
+
