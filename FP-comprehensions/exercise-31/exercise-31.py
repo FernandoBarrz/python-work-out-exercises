ec7ee42941db453c66af294f894d5f413b59a5b4
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-09 23:20:17 -0500

    Added solution for exercise-31

--- FP-comprehensions/exercise-31/exercise-31.py
+++ FP-comprehensions/exercise-31/exercise-31.py
@@ -2,20 +2,24 @@
 # Author: Barrios Ramirez Luis Fernando
 # Language: Python3 3.10.2 64-bit on Mac M1
 
-def pig_latin_from_file(file_name) -> str:
-    pig_lating_word: str = ''
+def pig_latin(words):
+    pig_lating_word = ""
     VOWELS = ('a', 'e', 'i', 'o', 'u')
-    with open(file_name) as file:
-        for line in file.readlines():
-            for word in line.split():
-                print(word)
-                if word[0] in VOWELS:
-                    pig_lating_word = word + 'way'
-                else:
-                    temp_first_letter = word[0]
-                    pig_lating_word = word[1:] + temp_first_letter + 'ay'
-
+    for word in words.split():
+        
+        if word[0] in VOWELS:
+            pig_lating_word = word + 'way'
+        else:
+            temp_first_letter = word[0]
+            pig_lating_word = word[1:] + temp_first_letter + 'ay'
     return pig_lating_word
+
+
+
+def pig_latin_from_file(file_name) -> str:
+    return " ".join(pig_latin(word_inner)
+                    for word in open(file_name) # Iterates through each line of filename
+                    for word_inner in word.split()) # Iterates through each word in the current line
 print(pig_latin_from_file("wcfile.txt"))
 
 
