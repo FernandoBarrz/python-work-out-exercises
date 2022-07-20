155c2e181f85b29a494534833bafec521d4ff29a
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-20 12:01:58 -0500

    Added exercise-43 solution

--- objects/exercise-43/exercise-43.py
+++ objects/exercise-43/exercise-43.py
@@ -4,14 +4,37 @@
 
 
 class Animal:
-    def __init__(self):
-        pass
+    def __init__(self, color, number_of_legs):
+        self.color = color
+        self.species = self.__class__.__name__
+        self.number_of_legs = number_of_legs
+
+
+    def __repr__(self) -> str:
+        return f'{self.color} {self.species}, {self.number_of_legs}'
+        
+
 
 class Sheep(Animal):
-    pass
-class Wolve(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 4)
+        
+class Wolf(Animal):
+    def __init__(self, color):
+        super().__init__(color, 4)
 class Snake(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 0)
 class Parrot(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 2)
+
+wolf = Wolf('Grey')
+sheep = Sheep("White")
+snake = Snake("brown")
+parrot = Parrot("Green")
+
+print(wolf)
+print(sheep)
+print(snake)
+print(parrot)
