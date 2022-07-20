e3dfaee88650cf095cd76dcf2c29f20092b07f55
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-07-19 23:43:16 -0500

    Added exercise-42 solution

--- objects/exercise-42/exercise-42.py
+++ objects/exercise-42/exercise-42.py
@@ -4,4 +4,24 @@
 
 
 class FlexibleDict(dict):
-    _
+    def __getitem__(self, key):
+        try:
+            if key in self:
+                pass
+            elif str(key) in self:
+                key = str(key)
+            elif int(key) in self:
+                key = int(key)
+            
+        except ValueError:
+            pass
+
+        return dict.__getitem__(self, key)
+
+new_dict = FlexibleDict()
+new_dict[1] = 'FERNANDO'
+new_dict[2] = 'PABLO'
+new_dict['3'] = 'MARCO'
+new_dict['4'] = 'ANTONIO'
+
+print(new_dict[4])
